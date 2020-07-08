75ab56f1a76737661fa969d70453c44bf2602098
Alexandr Smirnov <newlinefu@gmail.com>
2020-07-09 02:25:34 +0300

    Updates

---
+++ precache-manifest.da0fea767839514e52fa650d61f0e4b2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593744399738e2fde2db657a0bc1543f",
+    "url": "/deadline_manager/index.html"
+  },
+  {
+    "revision": "ef85a3c8afa7758fd104",
+    "url": "/deadline_manager/static/css/main.bcd18083.chunk.css"
+  },
+  {
+    "revision": "217816cd4ad940dad591",
+    "url": "/deadline_manager/static/js/2.1e681bf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deadline_manager/static/js/2.1e681bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef85a3c8afa7758fd104",
+    "url": "/deadline_manager/static/js/main.c9c3c5b7.chunk.js"
+  },
+  {
+    "revision": "63367484dc2b368dc3e0",
+    "url": "/deadline_manager/static/js/runtime-main.0f870e95.js"
+  },
+  {
+    "revision": "3645c95ee8ff109ae89ebec4a1182031",
+    "url": "/deadline_manager/static/media/1.3645c95e.jpg"
+  },
+  {
+    "revision": "4d38d30f898d74f8dc913530a5a17b90",
+    "url": "/deadline_manager/static/media/2.4d38d30f.jpg"
+  },
+  {
+    "revision": "5b01f2e09994d82c6c89ead8066c566c",
+    "url": "/deadline_manager/static/media/4.5b01f2e0.jpg"
+  },
+  {
+    "revision": "51ac57fa8fa0161e8880fa718802da13",
+    "url": "/deadline_manager/static/media/5.51ac57fa.jpg"
+  },
+  {
+    "revision": "3bcf4f580eae2399ceebdbd3d87a9e3f",
+    "url": "/deadline_manager/static/media/6.3bcf4f58.jpg"
+  },
+  {
+    "revision": "7dbfcfc2f616afeb8aeff53bbd86ee21",
+    "url": "/deadline_manager/static/media/7.7dbfcfc2.jpg"
+  },
+  {
+    "revision": "6d774a9b47d58a9659b3ae8e087c7e58",
+    "url": "/deadline_manager/static/media/8.6d774a9b.jpg"
+  }
+]);
